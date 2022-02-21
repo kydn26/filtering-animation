e3821497f615268d69b0c5b7b9beb6b5f752e610
Kevin Dávila Ninaquispe <kydn260596@gmail.com>
2022-02-20 23:54:54 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Filtered } from './components/Filtered';
+import { Movie } from './components/Movie';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  const fetchPopular = async () => {
+    const response = await fetch(
+      'https://api.themoviedb.org/3/movie/popular?api_key=e300c9898e1d3935bd4c1ef8541908e0&language=en-US&page=1'
+    );
+    const data = await response.json();
+    setPopular(data.results);
+    setFiltered(data.results);
+  };
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filtered
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+        layout
+        className="popular-movies"
+      >
+        <AnimatePresence>
+          {filtered.map((movie) => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,18 @@
+import { motion } from 'framer-motion';
+
+export const Movie = ({ movie }) => {
+  return (
+    <motion.div
+      layout
+      animate={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, scale: 0 }}
+      exit={{ opacity: 0, scale: 0 }}
+    >
+      <h2>{movie.title}</h2>
+      <img
+        src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path}
+        alt={movie.title}
+      />
+    </motion.div>
+  );
+};
